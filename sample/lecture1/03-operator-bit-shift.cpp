8c9485cbc741aea808c4b080b796b4cd78c86a84
6f8l <atsuki02jojo@gmail.com>
2020-05-18 15:35:12 +0900

    Add bit shift

---
+++ sample/lecture1/03-operator-bit-shift.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int c = 5;
+  cout << (c >> 1) << endl;
+  cout << (c >> 2) << endl;
+  cout << (c << 1) << endl;
+  cout << (c << 2) << endl;
+}
